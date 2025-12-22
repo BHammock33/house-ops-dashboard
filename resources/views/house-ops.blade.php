ff459c9fc331ff977357907d2141f33609a5480c
BHammock33 <BennettHammock@gmail.com>
2025-12-22 16:49:49 -0500

    typo

--- resources/views/house-ops.blade.php
+++ resources/views/house-ops.blade.php
@@ -2,8 +2,12 @@
 <html lang="en" data-user-id="{{ auth()->id() ?? 'guest' }}">
 <head>
   <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width, initaial-scale=1" />
-  <link rel="styleheet" href="{{ secure_asset('house-ops-assets/style.css')}}" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <title>House Ops</title>
+
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+
+  <link rel="stylesheet" href="{{ secure_asset('house-ops-assets/style.css') }}" />
 </head>
 <body>
   <main class="wrap">
